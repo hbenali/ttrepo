6b1e286e9a3bc04bd5368d47c2b6987f85df0e24
Houssem Ben Ali <hbenali.tn@gmail.com>
2024-09-15 15:40:27 +0200

    Fix vars

--- ghaagentdaemon.sh
+++ ghaagentdaemon.sh
@@ -2,7 +2,7 @@
 echo "Forwarding SSH Port to Jenkins agent"
 ssh-keygen -y -f ~/.ssh/id* >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
-ssh -g -N ${inputs.AGENT_HOST} -R ${AGENT_FORWARD_PORT}:localhost:22 &
+ssh -g -N ${AGENT_HOST} -R ${AGENT_FORWARD_PORT}:localhost:22 &
 SSH_PID=$!
 trap "kill -9 ${SSH_PID}" EXIT
 echo "Connected"
